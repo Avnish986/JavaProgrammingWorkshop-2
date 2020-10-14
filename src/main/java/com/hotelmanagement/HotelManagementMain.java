0d32b5ad81a0824a14784e850f0540c552df53c0
avnish986 <avnishgupta986@gmail.com>
2020-10-14 18:51:58 +0530

    [Avnish]Created: hotel management program welcome msg

---
+++ src/main/java/com/hotelmanagement/HotelManagementMain.java
@@ -0,0 +1,9 @@
+package com.hotelmanagement;
+
+public class HotelManagementMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to hotel reservation program");
+	}
+
+}
